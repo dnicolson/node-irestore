27ee6bf18684d80da9cce0c862adc6274976dce0
Dave Nicolson <david.nicolson@gmail.com>
2022-10-09 20:08:28 +0200

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export default class IRestore {
+  constructor(backupPath: string);
+  ls(domain: string): Promise<string>;
+  restore(domain: string, path: string): Promise<string>;
+  dumpKeys(outputFile: string): Promise<string>;
+  encryptKeys(inputFile: string, outputFile: string): Promise<string>;
+  apps(): Promise<string>;
+}
