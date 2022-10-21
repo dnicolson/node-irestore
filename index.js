b9997c4ddd06c32f1f7146c41ce151e0dfd25304
Dave Nicolson <david.nicolson@gmail.com>
2022-10-21 22:01:53 +0200

    Fix password input

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { spawn } = require('child_process');
 const os = require('os');
+const path = require('path');
 const pty = require('node-pty');
 const shellescape = require('shell-escape');
 
@@ -9,40 +10,59 @@ class IRestore {
     this.password = password;
   }
 
-  _runPtyProcess(bin, args, resolve) {
-    const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
-    const ptyProcess = pty.spawn(shell, [], {
-      name: 'xterm-color',
-      cols: 80,
-      rows: 30,
-      cwd: process.env.HOME,
-      env: process.env,
-    });
-    
-    let output;
-    ptyProcess.onData((data) => {
-      const lines = data.toString();
-      if (lines.endsWith('Backup Password: ')) {
-        ptyProcess.write(`${this.password}\n`);
-      }
+  _runPtyProcess(bin, args) {
+    return new Promise((resolve, reject) => {
+      const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+      const ptyProcess = pty.spawn(shell, [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 30,
+        cwd: process.env.HOME,
+        env: process.env,
+      });
+      
+      let output;
+      let timesDoneOutputted = 0;
+      let passwordEntered = false;
+      ptyProcess.onData((data) => {
+        const lines = data.toString();
 
-      output += lines;
-      if (lines.startsWith("\u001b[?2004") && output.length > 200) {
-        ptyProcess.kill();
-        resolve(output);
-      }
-    });
+        if (lines.endsWith('Backup Password: ')) {
+          ptyProcess.write(`${this.password}\n`);
+          passwordEntered = true;
+        }
 
-    ptyProcess.write(`${bin} ${shellescape(args)}\n`);
+        if (lines.includes('irestore done.') && timesDoneOutputted++ === 2) {
+          ptyProcess.kill();
+          if (passwordEntered) {
+            resolve(output);
+          } else {
+            reject('pty error, please try again.');
+          }
+        }
+
+        output += lines;
+      });
+
+      ptyProcess.write(`${bin} ${shellescape(args)}; echo 'irestore done.'\n`);
+    });
   }
 
-  _command(args) {
-    return new Promise((resolve) => {
-      const bin = `${process.env.PWD}/node_modules/.bin/irestore`;
+  runCommand(args) {
+    return new Promise(async (resolve, reject) => {
+      const bin = path.join(__dirname, 'node_modules/.bin/irestore')
       if (this.password) {
-        this._runPtyProcess(bin, args, resolve);
+        try {
+          const output = await this._runPtyProcess(bin, args);
+          resolve(output);
+        } catch(err) {
+          reject(err);
+        }
       } else {
         const child = spawn(bin, args, { stdio: ['inherit', 'inherit'] });
+        if (!child) {
+          return reject();
+        }
         child.on('exit', () => {
           resolve();
         });
@@ -51,23 +71,23 @@ class IRestore {
   }
 
   ls(domain) {
-    return this._command([this.backupPath, 'ls', domain]);
+    return this.runCommand([this.backupPath, 'ls', domain]);
   }
 
   restore(domain, path) {
-    return this._command([this.backupPath, 'restore', domain, path]);
+    return this.runCommand([this.backupPath, 'restore', domain, path]);
   }
 
   dumpKeys(outputFile) {
-    return this._command([this.backupPath, 'dumpkeys', outputFile]);
+    return this.runCommand([this.backupPath, 'dumpkeys', outputFile]);
   }
 
   encryptKeys(inputFile, outputFile) {
-    return this._command([this.backupPath, 'encryptkeys', inputFile, outputFile]);
+    return this.runCommand([this.backupPath, 'encryptkeys', inputFile, outputFile]);
   }
 
   apps() {
-    return this._command([this.backupPath, 'apps']);
+    return this.runCommand([this.backupPath, 'apps']);
   }
 }
 
